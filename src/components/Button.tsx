32fb46516c1c4783ce1306dc207e7652e6a8e79f
genaroibc <bonavitaismael@gmail.com>
2023-05-25 22:45:32 -0300

    [FEAT]: add Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,17 @@
-export function Button() {
-  return <button>Click me</button>
+type ButtonVariant = 'primary' | 'secondary'
+
+const baseClassName =
+  'rounded-md border-2 py-2 px-4 border-blue-500 hover:border-blue-400 text-white'
+
+const classNames: Record<ButtonVariant, string> = {
+  primary: 'bg-blue-500 hover:bg-blue-400',
+  secondary: 'bg-transparent'
+}
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: ButtonVariant
+}
+
+export function Button(props: ButtonProps) {
+  return <button {...props} className={`${baseClassName} ${classNames[props.variant]}`} />
 }
