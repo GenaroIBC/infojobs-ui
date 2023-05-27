2158449adeb9d2356c0d1ec4a03b9ee7d077c677
genaroibc <bonavitaismael@gmail.com>
2023-05-27 14:13:49 -0300

    [FEAT]: add support for different button colors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,63 @@
 type ButtonVariant = 'primary' | 'secondary'
+type ButtonColor = 'blue' | 'darkblue' | 'orange' | 'gray' | 'green' | 'red' | 'yellow'
 
-const BASE_CLASSNAME =
-  'rounded-md border-2 py-2 px-4 hover:border-infojobs-blue border-infojobs-darkblue'
+const BASE_CLASSNAME = 'rounded-md border-2 py-2 px-4'
 
-const CLASSNAMES: Record<ButtonVariant, string> = {
-  primary: 'hover:bg-infojobs-blue bg-infojobs-darkblue text-white',
-  secondary:
-    'bg-transparent hover:text-infojobs-blue text-infojobs-darkblue hover:text-infojobs-blue'
+const VARIANT_CLASSNAMES: Record<ButtonVariant, string> = {
+  primary: 'text-white',
+  secondary: ''
+}
+
+const COLOR_CLASSNAMES: Record<ButtonColor, string> = {
+  blue: 'border-infojobs-blue',
+  darkblue: 'border-infojobs-darkblue',
+  gray: 'border-infojobs-gray',
+  green: 'border-infojobs-green',
+  orange: 'border-infojobs-orange',
+  red: 'border-infojobs-red',
+  yellow: 'border-infojobs-yellow'
 }
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant: ButtonVariant
+  variant?: ButtonVariant
+  color?: ButtonColor
 }
 
 export function Button(props: ButtonProps) {
-  return <button {...props} className={`${BASE_CLASSNAME} ${CLASSNAMES[props.variant]}`} />
+  const { variant = 'primary', color = 'blue' } = props
+
+  const buttonBackgroundColor =
+    variant === 'primary'
+      ? `bg-infojobs-${color} text-white`
+      : `bg-transparent text-infojobs-${color}`
+
+  console.log({ [color]: buttonBackgroundColor })
+  return (
+    <button
+      {...props}
+      className={`${BASE_CLASSNAME} ${VARIANT_CLASSNAMES[variant ?? 'primary']} ${
+        COLOR_CLASSNAMES[color ?? 'blue']
+      } ${buttonBackgroundColor}`}
+    />
+  )
 }
+
+export const btnBackgrounds = [
+  'bg-infojobs-blue',
+  'bg-infojobs-gray',
+  'bg-infojobs-darkblue',
+  'bg-infojobs-orange',
+  'bg-infojobs-green',
+  'bg-infojobs-yellow',
+  'bg-infojobs-red'
+]
+
+export const btnColors = [
+  'text-infojobs-blue',
+  'text-infojobs-darkblue',
+  'text-infojobs-gray',
+  'text-infojobs-green',
+  'text-infojobs-orange',
+  'text-infojobs-red',
+  'text-infojobs-yellow'
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     extend: {
       colors: {
         'infojobs-blue': '#167DB7',
-        'infojobs-darkblue': '#1972a3'
+        'infojobs-darkblue': '#1972a3',
+        'infojobs-orange': '#ff6340',
+        'infojobs-gray': '#999999',
+        'infojobs-green': '#00a550',
+        'infojobs-red': '#e93e40',
+        'infojobs-yellow': '#f1c40f'
+      },
+      maxWidth: {
+        'page-max-width': 'var(--page-max-width)'
       }
     }
   },
