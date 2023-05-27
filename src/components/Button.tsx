f47081a90546850322055271abf2997c80025d78
genaroibc <bonavitaismael@gmail.com>
2023-05-27 14:26:01 -0300

    [REFACTOR]: simplify classNames object

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,19 +3,35 @@ type ButtonColor = 'blue' | 'darkblue' | 'orange' | 'gray' | 'green' | 'red' | '
 
 const BASE_CLASSNAME = 'rounded-md border-2 py-2 px-4'
 
-const VARIANT_CLASSNAMES: Record<ButtonVariant, string> = {
-  primary: 'text-white',
-  secondary: ''
-}
-
-const COLOR_CLASSNAMES: Record<ButtonColor, string> = {
-  blue: 'border-infojobs-blue',
-  darkblue: 'border-infojobs-darkblue',
-  gray: 'border-infojobs-gray',
-  green: 'border-infojobs-green',
-  orange: 'border-infojobs-orange',
-  red: 'border-infojobs-red',
-  yellow: 'border-infojobs-yellow'
+const BUTTON_CLASSNAMES: Record<ButtonColor, Record<ButtonVariant, string>> = {
+  blue: {
+    primary: 'bg-infojobs-blue',
+    secondary: 'text-infojobs-blue border-infojobs-blue'
+  },
+  darkblue: {
+    primary: 'bg-infojobs-darkblue',
+    secondary: 'text-infojobs-darkblue border-infojobs-darkblue'
+  },
+  gray: {
+    primary: 'bg-infojobs-gray',
+    secondary: 'text-infojobs-gray border-infojobs-gray'
+  },
+  green: {
+    primary: 'bg-infojobs-green',
+    secondary: 'text-infojobs-green border-infojobs-green'
+  },
+  orange: {
+    primary: 'bg-infojobs-orange',
+    secondary: 'text-infojobs-orange border-infojobs-orange'
+  },
+  red: {
+    primary: 'bg-infojobs-red',
+    secondary: 'text-infojobs-red border-infojobs-red'
+  },
+  yellow: {
+    primary: 'bg-infojobs-yellow',
+    secondary: 'text-infojobs-yellow border-infojobs-yellow'
+  }
 }
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -26,38 +42,5 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export function Button(props: ButtonProps) {
   const { variant = 'primary', color = 'blue' } = props
 
-  const buttonBackgroundColor =
-    variant === 'primary'
-      ? `bg-infojobs-${color} text-white`
-      : `bg-transparent text-infojobs-${color}`
-
-  console.log({ [color]: buttonBackgroundColor })
-  return (
-    <button
-      {...props}
-      className={`${BASE_CLASSNAME} ${VARIANT_CLASSNAMES[variant ?? 'primary']} ${
-        COLOR_CLASSNAMES[color ?? 'blue']
-      } ${buttonBackgroundColor}`}
-    />
-  )
+  return <button {...props} className={`${BASE_CLASSNAME} ${BUTTON_CLASSNAMES[color][variant]}`} />
 }
-
-export const btnBackgrounds = [
-  'bg-infojobs-blue',
-  'bg-infojobs-gray',
-  'bg-infojobs-darkblue',
-  'bg-infojobs-orange',
-  'bg-infojobs-green',
-  'bg-infojobs-yellow',
-  'bg-infojobs-red'
-]
-
-export const btnColors = [
-  'text-infojobs-blue',
-  'text-infojobs-darkblue',
-  'text-infojobs-gray',
-  'text-infojobs-green',
-  'text-infojobs-orange',
-  'text-infojobs-red',
-  'text-infojobs-yellow'
-]
