94b416b0c511120bf8fb436e33059570b8e703e0
genaroibc <bonavitaismael@gmail.com>
2023-05-28 23:36:53 -0300

    [FIX]: improve accessibility for toggle component

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,18 +1,28 @@
+import { useId } from 'react'
+
 interface ToggleProps {
   onToggle: (checked: boolean) => void
 }
 
 export function Toggle({ onToggle }: ToggleProps) {
+  const inputID = useId()
+
   return (
-    <label className="relative inline-flex items-center cursor-pointer">
+    <label
+      tabIndex={0}
+      htmlFor={inputID}
+      className="relative inline-flex items-center cursor-pointer"
+    >
       <input
         onChange={(event) => {
           onToggle(event.target.checked)
         }}
         type="checkbox"
+        name={inputID}
+        id={inputID}
         className="sr-only peer"
       />
-      <div className="w-11 h-6 bg-gray-300 rounded-full peer peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-gray-50 after:rounded-full after:h-5 after:w-5 after:transition-all border-2 border-gray-400 peer-checked:border-infojobs-darkblue peer-checked:bg-infojobs-darkblue"></div>
+      <div className="w-11 h-6 bg-gray-300 rounded-full peer peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-gray-50 after:rounded-full after:h-5 after:w-5 after:transition-all border border-gray-400 peer-checked:border-infojobs-darkblue peer-checked:bg-infojobs-darkblue"></div>
     </label>
   )
 }
